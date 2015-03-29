0366a6db16c543aa6f193401fe3a8f387456ba2a
Shubham Varshney <shubham94.varshney@gmail.com>
2015-03-29 17:52:02 +0530

    debig=true

--- myfirst/myfirst/settings.py
+++ myfirst/myfirst/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'ex)ad%f@7fa56p2i#rkambizfmpgz8kztneltc$h-ykq-1b9+1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =False 
+DEBUG =True
 
 TEMPLATE_DEBUG = True
 
